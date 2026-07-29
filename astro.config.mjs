f688cf0683cebe91f2db6a8b5f7134db07f8eb24
Chaoli <642600539@qq.com>
2026-07-29 23:04:01 +0800

    chore: 引入Svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [],
+  integrations: [svelte()],
 
   vite: {
     plugins: [tailwindcss()],
